096025251436ae56116994496b12f210b41165ff
Alux <plutocoding@gmail.com>
2024-01-12 22:47:28 -0500

    trying to seed db

--- db/schema.sql
+++ db/schema.sql
@@ -17,4 +17,21 @@ INSERT INTO category (category_name) VALUES
   ('Books'),
   ('Clothes');
 
-SELECT * FROM category;
+-- Setup the info for individual products
+CREATE TABLE product (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  price DECIMAL(12, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES category(category_id)
+);
+
+INSERT INTO product (product_name, price, stock, category_id) VALUES
+  ('Crimson Gladiator', 9.99, 25, 1),
+  ('How to Rule the World - For Dummies', 15, 10, 2),
+  ('Wolf-Grey Jordan 5', 349.99, 3, 3);
+
+-- SELECT * FROM category;
+SELECT * FROM product;
--- models/index.js
+++ models/index.js
@@ -1,20 +1,32 @@
-// import models
-const Product = require('./Product');
-const Category = require('./Category');
-const Tag = require('./Tag');
-const ProductTag = require('./ProductTag');
+const Tag = require('./tag');
+const Product = require('./product');
+const Category = require('./category');
+const ProductTag = require('./productTag');
 
-// Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
-// Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
 
-// Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+});
 
-// Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+});
 
-module.exports = {
-  Product,
-  Category,
-  Tag,
-  ProductTag,
-};
+ProductTag.belongsTo(Product, {
+  foreignKey: 'product_id',
+});
+
+ProductTag.belongsTo(Tag, {
+  foreignKey: 'tag_id',
+});
+
+module.exports = { Category, Product, Tag, ProductTag };

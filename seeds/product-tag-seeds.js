cbe9acbee2a5ad139cb801a7a437ae2a1be9da55
Alux <plutocoding@gmail.com>
2024-01-13 00:32:28 -0500

    connecting tag seeds

--- seeds/product-tag-seeds.js
+++ seeds/product-tag-seeds.js
@@ -1,56 +1,17 @@
-const { ProductTag } = require('../models');
+//We want to seed data in our database by associating products with tags using the ProductTag model and bulk insertion. 
+// The exported productTagData array so that we can use it elsewhere.
+
+const { ProductTag, Product, Tag } = require(`../models`);
 
 const productTagData = [
-  {
-    product_id: 1,
-    tag_id: 6,
-  },
-  {
-    product_id: 1,
-    tag_id: 7,
-  },
-  {
-    product_id: 1,
-    tag_id: 8,
-  },
-  {
-    product_id: 2,
-    tag_id: 6,
-  },
-  {
-    product_id: 3,
-    tag_id: 1,
-  },
-  {
-    product_id: 3,
-    tag_id: 3,
-  },
-  {
-    product_id: 3,
-    tag_id: 4,
-  },
-  {
-    product_id: 3,
-    tag_id: 5,
-  },
-  {
-    product_id: 4,
-    tag_id: 1,
-  },
-  {
-    product_id: 4,
-    tag_id: 2,
-  },
-  {
-    product_id: 4,
-    tag_id: 8,
-  },
-  {
-    product_id: 5,
-    tag_id: 3,
-  },
+    {product_id: 1, tag_id: 1, },
+    {product_id: 2, tag_id: 2, },
+    {product_id: 3, tag_id: 3, },
 ];
 
-const seedProductTags = () => ProductTag.bulkCreate(productTagData);
+ProductTag.bulkCreate(productTagData, {
+    include: [{ model: Product }],
+    include: [{ model: Tag }],
+});
 
-module.exports = seedProductTags;
+module.exports = productTagData;
--- seeds/tag-seeds.js
+++ seeds/tag-seeds.js
@@ -14,7 +14,8 @@ const productTagData = [
     { product_id: 2, tag_id: 2 },
     { product_id: 3, tag_id: 3 },
   ];
-
+// Calling productTagData here, which comes from our product-tag-seeds.js file
+// Required at the top of the file for this connection to work
 Tag.bulkCreate(tagData);
 ProductTag.bulkCreate(productTagData, {
     include: [{ model: Product }]

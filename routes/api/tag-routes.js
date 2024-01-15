afac96cdb15686cc4fbf9783796fe756189a1186
Alux <plutocoding@gmail.com>
2024-01-15 00:29:28 -0500

    Trying to setup api tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,28 +1,54 @@
-const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const router = express.Router();
+const express = require('express');
+const { getTags } = require('../../helper');
+const { Tag, ProductTag, Product } = require(`../../models`);
 
-// The `/api/tags` endpoint
+router.get(`/tags`, async ( req, res ) => getTags(req, res));
 
-router.get('/', (req, res) => {
-  // find all tags
-  // be sure to include its associated Product data
+router.get(`/tags/:id`, async ( req, res ) => {
+    try{
+        const tagID = req.params.id;
+        const tag = await Tag.findOne({ 
+            where: { id: tagID }, 
+            include: [{model: Product, through: ProductTag }], 
+        });
+        if (tag) {
+            res.json(tag);
+        } else {
+            res.status(404).json({ error: `tag not found` });
+        }
+    } catch (error) {
+        console.log(`error getting tag by ID`. error);
+        res.status(500).json({ error: `server error` });
+    }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.post(`/tags`, async ( req, res ) => {
+    try{
+        const newTag = await Tag.create(  req.body );
+        res.status(201).json(newTag);
+    } catch (error) {
+        console.log(`error creating new tag`, error);
+        res.status(400).json({ error: `invalid data input` });
+    }
 });
 
-router.post('/', (req, res) => {
-  // create a new tag
-});
-
-router.put('/:id', (req, res) => {
-  // update a tag's name by its `id` value
-});
-
-router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
-});
+router.delete(`/tags/:id`, async (req, res) => {
+    try {
+      const tagID = req.params.id;
+      const deletedRows = await Tag.destroy({
+        where: { id: tagID },
+      });
+      if (deletedRows > 0) {
+        res.status(200).json({ message: 'Your Tag was deleted' });
+      } else {
+        res.status(404).json({ error: 'No tag found' });
+      }
+    } catch (error) {
+      console.log('Error deleting tag by ID:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  });
+  
 
 module.exports = router;
